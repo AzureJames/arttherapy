1b1ee9feaa7078643e0b57d2a572442db1fd5b2a
AzureJames <azurethegreat@gmail.com>
2024-06-10 20:25:33 -0600

    june10 bios etc

--- src/components/DevBanner.js
+++ src/components/DevBanner.js
@@ -9,14 +9,17 @@ export default function DevBanner({title, tagline, tagline2, image, svgColor}) {
     <>
         <div class="flex-800">
             <div  className='devbanner' style={{display:"flex", justifyContent: "center", flexWrap: "wrap", alignItems: "end", margin: "1rem"}}>
-              <img src={hvac} width={575} height={600} alt="diving painting" style={{flex: "1 1 700px"}}/>
+              <img src={hvac} width={575} height={600} style={{height:'fit-content', flex: "1 1 700px"}} alt="diving painting"/>
               <caption style={{flex: "0 1 525px", marginBottom:'4rem'}}><p>“Swimming in the ocean is not just spending time in the water; it's a therapeutic dance with the waves that rejuvenates both body and soul.” (Digital water color painting)</p></caption>
             </div>
             <div  className='devbanner' style={{ margin: "1rem", alignSelf: "center" }}>
               <h1 className='title'>{title}</h1>
+              <div className='bio-flex' style={{marginBottom:'1rem'}}>
+                <img src={lynette} alt={"Lynette Portrait"} style={{maxWidth: "300px"}}></img>
+                <p className='description' style={{fontSize: '19px', fontWeight: 'bold'}}>{tagline}</p>
+              </div>
               <div className='bio-flex'>
                 <img src={lynette} alt={"Lynette Portrait"} style={{maxWidth: "300px"}}></img>
-                <p className='description'>{tagline}</p>
                 <p className='description' style={{fontSize: '19px', fontWeight: 'bold'}}>{tagline2}</p>
               </div>
             </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -29,34 +29,40 @@ function Home() {
 	return (
 		<div className='home'>
 								<Navbar />
+			<div style={{display: 'flex', alignItems: 'center', flexDirection: 'column', marginBlock: '1rem'}}>
+				<h1 className='off-white'   style={{ marginBlock: '0rem', width:'fit-content'}}>Life is Art Creative Arts Therapy, PLLC</h1>
+				<p className='off-white'  style={{ marginBottom: '2rem', marginTop:"1rem"}}>Lynette Lex, PhD, ATR-BC, LCAT, CASAC</p>
+			</div>
 			<div className='flex-md' style={{maxWidth:"1200px"	, marginInline:"auto", marginTop: "1.6rem"}}>
-				{/* <div className='bg'> */}
-					{/* <BannerCard
-						index='text'
-						title='Cozy Bros'
-						body='We keep you warm'
-						body2='We keep you warm'
-					></BannerCard> */}
-					<img src={whitePainting} alt="abstract art" style={{width: '100%', maxWidth: '400px', height: 'auto'}}></img>
-					{/* 
-					
-					<div>
-						<h1 className='rate-title'>rate title</h1>
-						<HeroBanner />
-					</div> */}
-				{/* </div> */}
-				<div className='inner-container' style={{marginInline: '1rem', marginBottom: '3rem', padding: "1.5rem"}}>
-					<h1 className='off-white'   style={{ marginBlock: '0rem'}}>Life is Art Creative Arts Therapy, PLLC</h1>
-					<p className='off-white'  style={{ marginBottom: '2rem', marginTop:"1rem"}}>Lynette Lex, PhD, ATR-BC, LCAT, CASAC</p>
+					<img src={whitePainting} alt="abstract art" style={{width: 'auto', maxWidth: '420px', height: 'auto', maxHeight:'580px'}}></img>
+				<div className='inner-container' style={{marginInline: '1rem', marginBottom: '3rem', paddingInline: "1.5rem"}}>
 					<div style={{ marginBottom: '2rem', backgroundColor:'rgb(10,30,53',borderRadius:'4px',padding:'1rem'}}>
+					<h2 style={{ fontSize: '29px'}}>What You Can Expect</h2>
 					<p className='off-white m-w65' style={{ marginBottom: '2rem'}}>
-					Life is Art is an unique Art Therapy Studio space, where Art Therapy, EMDR and a holistic wellness approach to therapy are combined. Meditation, Mindfulness, Reiki and Aromatherapy are often included in sessions. 
+					At Life is Art, we strive to create a space for you to feel seen and heard, no
+matter the challenges you are currently facing. Within the first few
+sessions our hope is that you will begin to experience a sense of relief or
+the ability to breathe a little easier, knowing you have someone on your
+side to partner with and ready to travel with you through all you are working on or dealing
+with.
 					</p>
+					<h2 style={{ fontSize: '29px'}}>People Who Can Benefit</h2>
 					<p className='off-white m-w65'  style={{ marginBottom: '2rem'}}>
-					Art Therapy gives a nonverbal means of expression when words are not enough. This is a safe and inviting atmosphere where trauma can be addressed and healing is supported.  
+					While your experience is unique, please allow us to share our expertise in
+areas such as trauma, emotional regulation, anxiety, depression, grief and loss, life transitions,
+substance use and substance use in the family. If you are someone who has been interested in
+reconnecting with your body or soul this is very likely the place to
+rediscover the mind body connection in your life.
 					</p> 
+					<h2 style={{ fontSize: '29px'}}>About Us</h2>
 					<p className='off-white m-w65'  style={{ marginBottom: '2rem'}}>
-					In-person and telehealth appointments are available. Private pay and some insurances accepted. 
+					Life is Art is an unique Art Therapy Studio space, where Art Therapy, EMDR
+and a holistic wellness approach to therapy are all combined. Meditation,
+Mindfulness, Reiki and Aromatherapy are often included in sessions. Art
+Therapy gives a non-verbal means for expression when words are not
+enough. EMDR allows a process of resolving traumas both large and small. This is a safe and
+inviting atmosphere where trauma can be addressed and healing is possible. In person and
+telehealth appointments are available. Self-Pay fee and some insurances are accepted.
 					</p>
 					</div>
 					{/* <div style={{display:"flex", gap:".5rem"}}>
@@ -72,12 +78,20 @@ function Home() {
 			{/* HERO BANNER FROM BLC??
 			CURVY BANNER */}
 			<DevBanner
-				title='Bio'
-				tagline='' //The trees are as precious to us as diamonds, pearls, and the petals of a white rose. Quickbooks Online provides the way to paperless bookkeeping and we will donate $25 to Re-Tree NY for each new client.
-				tagline2='Lynette Lex is a Licensed Creative Art Therapist, Registered and Board
-				Certified Art Therapist. She is a master credentialed alcohol and substance
-				abuse counselor along with having a Doctorate in Holistic Health. Lynette is also certified through EMDRIA in EMDR. She is a Reiki master, a
-				SoulCollage® Facilitator and a MARI facilitator/teacher.'
+				title='Bios'
+				tagline='Lynette Lex is a Licensed Creative Art Therapist, Registered and Board
+Certified Art Therapist with 27 years of experience. She is a master credentialed alcohol and
+substance abuse counselor along with having a Doctorate in Holistic Health.
+Lynette is also certified through EMDRIA in EMDR. She is a Reiki master, a
+SoulCollage® Facilitator and a MARI facilitator/teacher. Lynette is warm and engaging, allowing
+for a comfortable therapeutic relationship to explore even the most difficult topics.' //The trees are as precious to us as diamonds, pearls, and the petals of a white rose. Quickbooks Online provides the way to paperless bookkeeping and we will donate $25 to Re-Tree NY for each new client.
+				tagline2='Danielle Walters is a licensed Creative Arts Therapist along with a credentialed alcohol and
+substance use counselor. She has 10 years in the field and a passion for helping people
+navigate life&#39;s twists and turns. In her sessions, you will explore the depths of your emotions,
+thoughts and experiences using a variety of therapeutic approaches tailored to your unique
+needs. Danielle believes in the power of expressive arts to heal and transform. Whether you
+prefer to express yourself through words or creative outlets such as drawing, painting or writing
+she is here to support you every step of the way.'
 				style={{marginBottom:'0px'}}
 			></DevBanner>
 							{/* <OurServices /> */}
